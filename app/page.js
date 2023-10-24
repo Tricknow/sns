1727ba0103a224c8b2a365b441bb5b5dc14d8bfe
Tricknow <argogy@naver.com>
2023-10-24 11:46:39 +0900

    배포연습

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      배포를 성공했습니다 by argogy
+      배포를 성공했습니다 11:46 by argogy
     </main>
   )
 }
